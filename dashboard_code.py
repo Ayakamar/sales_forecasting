119057dd4ead626bb6461fbc8103d8355ea45ea6
Ayakamar <132248526+Ayakamar@users.noreply.github.com>
2025-05-09 19:08:10 +0300

    Update dashboard_code.py

--- dashboard_code.py
+++ dashboard_code.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1ymH5Fr0ka0tLsiGRZ-MonkJszFstspjZ
 """
 
-!pip install streamlit
+
 
 import streamlit as st
 import pandas as pd
